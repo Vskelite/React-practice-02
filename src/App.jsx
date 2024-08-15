9001ccbff9d56f1e2eb68a99b17e106e0e842e09
vivek khamkar <khamkarv01@gmail.com>
2024-08-15 23:12:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const increaseCount = ()=>{
+    useEffect ( ()=> {
+      setTimeout( ()=>{
+        setCount( count => count +1)},2000)},[count]
+               }
+  }
   return (
     <>
+      <h1>Count: {count}</h1>
+      <button onclick ={increaseCount}>Click</button>
     </>
   )
 }
