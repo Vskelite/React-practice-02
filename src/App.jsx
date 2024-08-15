3993a76c071647116d56be3add2bdd0e9f018f9d
vivek khamkar <khamkarv01@gmail.com>
2024-08-15 23:19:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <h1>Count: {count}</h1>
-      <button onclick ={increaseCount}>Click</button>
+      <button onClick ={increaseCount}>Click</button>
     </>
   )
 }
