a8782457085863793d20aced549e927f596e8f80
vivek khamkar <khamkarv01@gmail.com>
2024-10-26 21:50:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const increaseCount = ()=>{
     useEffect ( ()=> {
       setTimeout( ()=>{
